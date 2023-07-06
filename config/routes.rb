abc99ea2a3e4609f846539aab05a66507f0afa86
EC2 Default User <ec2-user@ip-172-31-36-34.ap-northeast-1.compute.internal>
2023-07-06 08:12:59 +0000

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  
 
+devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
-  devise_for :admins
-  devise_for :customers
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about', as: 'about'
