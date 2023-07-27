52a1ad25c1df06e2b9fb71386ffd7d38015fbf98
EC2 Default User <ec2-user@ip-172-31-36-34.ap-northeast-1.compute.internal>
2023-07-27 09:42:59 +0000

    レイアウト修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,25 @@
 class Public::FavoritesController < ApplicationController
-    def create
+  before_action :authenticate_customer! # ログインしていない場合はログイン画面にリダイレクトする
+
+  def create
     post = Post.find(params[:post_id])
     favorite = current_customer.favorites.new(post_id: post.id)
-    favorite.save
-    redirect_to post_path(post)
+
+    if favorite.save
+      redirect_to post_path(post), notice: 'いいねしました。'
+    else
+      redirect_to post_path(post), alert: 'いいねに失敗しました。'
     end
+  end
 
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_customer.favorites.find_by(post_id: post.id)
-    favorite.destroy
-    redirect_to post_path(post)
+
+    if favorite.destroy
+      redirect_to post_path(post), notice: 'いいねを解除しました。'
+    else
+      redirect_to post_path(post), alert: 'いいねの解除に失敗しました。'
+    end
   end
 end
